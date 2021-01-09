47b5bfdd83317e91b96570b4ff9de8a8c7ac0ddc
Datenstrudel <twx1@gmx.de>
2021-01-09 14:57:26 +0100

    Implements builder for FixtureFactory

---
+++ src/main/kotlin/net/datenstrudel/fixtmagic/FixtureFactory.kt
@@ -0,0 +1,285 @@
+package net.datenstrudel.fixtmagic
+
+import org.slf4j.LoggerFactory
+import java.lang.RuntimeException
+import java.time.Instant
+import java.time.ZoneId
+import kotlin.random.Random
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.KTypeParameter
+import kotlin.reflect.jvm.isAccessible
+import kotlin.reflect.jvm.jvmErasure
+import kotlin.reflect.jvm.jvmName
+import kotlin.reflect.typeOf
+
+class FixtureFactory private constructor(
+    val randomIntRange: IntRange,
+    val randomStringLength: Int = 10,
+    val randomArrayLength: Int = 10,
+    val randomCollectionSize: Int = 10
+) {
+
+    private constructor(builder: Builder) : this(
+        randomIntRange = builder.randomIntRange,
+        randomStringLength = builder.randomStringLength,
+        randomArrayLength = builder.randomArrayLength,
+        randomCollectionSize = builder.randomCollectionSize
+    )
+
+    private val rand = Random
+
+    @OptIn(ExperimentalStdlibApi::class)
+    inline fun <reified T> createInstance(): T{
+        return createInstance(typeOf<T>(), null, null) as T
+    }
+
+    fun createInstance(type: KType, typeParams: Map<KTypeParameter, KType?>? = null, paramName: String? = null): Any {
+        val resTypeParams = determineAndMergeNewTypeParams(type, typeParams)
+
+        return createInstance(type.classifier as KClass<*>, type, resTypeParams, paramName)
+    }
+
+    private fun determineAndMergeNewTypeParams(type: KType, parentTypeParams: Map<KTypeParameter, KType?>?, ): Map<KTypeParameter, KType?> {
+        val resTypeParams = mutableMapOf<KTypeParameter, KType?>()
+
+        // Create a type param -> actual runtime type mapping
+        val newTypeParams = type.jvmErasure.typeParameters.zip(
+            type.arguments.map { it.type }
+        ).toMap()
+
+        parentTypeParams?.let { resTypeParams.putAll(it) }
+
+        newTypeParams.forEach { (k, v) ->
+            val newValue = resTypeParams[v!!.classifier] ?: v  // Take actual (parent's) type param down to nested usage
+            resTypeParams[k] = newValue
+        }
+        return resTypeParams
+    }
+
+    private fun createInstance(clazz: KClass<*>, type: KType, typeParams: Map<KTypeParameter, KType?>? = null, paramName: String? = null): Any{
+        val primitive = createStandardInstanceOrNull(clazz, type, paramName)
+        primitive?.let { return it }
+
+        val constructors = clazz.constructors
+            .sortedBy { it.parameters.size }
+
+        var lastThrow: Throwable? = null
+        for (constructor in constructors) {
+            try {
+                val arguments = constructor.parameters
+                    .map { it.type to it.name }
+                    .map {
+                        // look up specific type for generic parameter or, if non generic, take the type as is
+                        val typeToCreate = typeParams?.get(it.first.classifier) ?: it.first
+                        createInstance(typeToCreate, typeParams, it.second)
+                    }
+                    .toTypedArray()
+                    constructor.isAccessible = true
+                    return constructor.call(*arguments)
+            } catch (e: Throwable) {
+                lastThrow = e
+                log.debug("Couldn't create instance", e)
+            }
+        }
+        throw lastThrow ?: NoUsableConstructorFound("Couldn't create instance of type=${clazz.jvmName}")
+    }
+
+    private fun createStandardInstanceOrNull(clazz: KClass<*>, type: KType, paramName: String?): Any? {
+        var res: Any? = when (clazz) {
+            // Primitives
+            Boolean::class -> rand.nextBoolean()
+            Byte::class -> rand.nextBytes(1)[0]
+            Char::class -> rand.nextInt(32, 126).toChar()
+            Short::class -> rand.nextInt(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt()).toShort()
+            Int::class -> rand.nextInt(randomIntRange.first, randomIntRange.last)
+            Long::class -> rand.nextLong()
+            Float::class -> rand.nextFloat()
+            Double::class -> rand.nextDouble()
+            String::class -> createRandomString(paramName)
+
+            // Arrays
+            BooleanArray:: class -> createBooleanArray(type)
+            ByteArray::class -> createByteArray(type)
+            CharArray::class -> createCharArray(type)
+            ShortArray::class -> createShortArray(type)
+            IntArray::class -> createIntArray(type)
+            LongArray::class -> createLongArray(type)
+            FloatArray::class -> createFloatArray(type)
+            DoubleArray::class -> createDoubleArray(type)
+
+            // Collections
+            Map::class -> createRandomMap(type, paramName)
+            Set::class -> createRandomList(type, paramName).toSet()
+            List::class, Collection::class -> createRandomList(type, paramName)
+
+            // Java Time
+            ZoneId::class -> ZoneId.systemDefault()
+            Instant::class -> Instant.ofEpochMilli(rand.nextLong())
+
+            else -> null
+        }
+
+        // not all types can be matched by KClass representation
+        res = res ?: when (clazz.qualifiedName ?: "") {
+            "kotlin.Array" -> createGenericArray(type, paramName)
+            else -> null
+        }
+
+        return res
+    }
+
+    private fun createRandomString(paramName: String?): String {
+        val paramLength = paramName?.length ?: 0
+        val randLength = this.randomStringLength - paramLength
+        if (randLength < 1) throw InvalidFixtureConfigException("randomStringLength too small for random string generation")
+
+        val res = (1 until randLength)
+            .map { rand.nextInt(32, 126).toChar() }
+            .joinToString(separator = "") { "$it" }
+
+        return paramName?.let { it + "_" + res } ?: res
+    }
+
+    private fun createRandomMap(type: KType, paramName: String?): Map<Any?, Any?> {
+        val keyType = type.arguments[0].type!!
+        val valType = type.arguments[1].type!!
+
+        val keys = (1..randomCollectionSize).map { createInstance(keyType, null, paramName) }
+        val values = (1..randomCollectionSize).map { createInstance(valType, null, paramName) }
+
+        return mutableMapOf(* keys.zip(values).toTypedArray())
+    }
+
+    private fun createRandomList(type: KType, paramName: String?): List<Any?> {
+        val elemType = type.arguments[0].type!!
+        return (1..randomCollectionSize)
+            .map { createInstance(elemType, null, paramName) }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createBooleanArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Boolean>()) as Boolean }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toBooleanArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createByteArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Byte>()) as Byte }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toByteArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createCharArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Char>()) as Char }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toCharArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createShortArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Short>()) as Short }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toShortArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createIntArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Int>()) as Int }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toIntArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createLongArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Long>()) as Long }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toLongArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createFloatArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Float>()) as Float }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toFloatArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun createDoubleArray(type: KType): Any {
+        val elements = (1..randomArrayLength).map { createInstance(typeOf<Double>()) as Double }
+
+        return if (type.arguments.isEmpty()) {
+            elements.toDoubleArray()
+        } else {  // an Array<Int> has an argument
+            elements.toTypedArray()
+        }
+    }
+
+    private fun createGenericArray(type: KType, paramName: String?): Any {
+        val elemType = type.arguments[0].type!!
+        val elements = (1..randomArrayLength).map { createInstance(elemType, null, paramName) }
+
+        return toTypedArray(elements, elemType)
+    }
+
+    /**
+     * The `list` argument supplied usually lacks the type of the generic argument(s), which makes
+     * `kotlin.Collection.toTypedArray()` return an `Array<Object>`. This function allows to create the correctly typed
+     * array by using the supplied `elemType`
+     */
+    private fun toTypedArray(list: List<Any>, elemType: KType): Any {
+        val resArray: Array<Any> =
+            java.lang.reflect.Array.newInstance((elemType.classifier as KClass<*>).java, randomArrayLength) as Array<Any>
+        list.forEachIndexed{ i, it -> resArray[i] = it }
+        return resArray
+    }
+
+
+    class Builder(
+        var randomIntRange: IntRange = IntRange(0, Int.MAX_VALUE),
+        var randomStringLength: Int = 10,
+        var randomArrayLength: Int = 10,
+        var randomCollectionSize: Int = 10
+    ) {
+        fun build() = FixtureFactory(this)
+    }
+
+    companion object {
+        val log = LoggerFactory.getLogger(this::class.java)
+
+        inline fun build(block: Builder.() -> Unit) = Builder().apply(block).build()
+    }
+}
+
+class NoUsableConstructorFound(msg: String) : RuntimeException(msg)
+class InvalidFixtureConfigException(msg: String) : RuntimeException(msg)
---
+++ src/test/kotlin/net/datenstrudel/fixtmagic/FixtureFactoryTest.kt
@@ -0,0 +1,472 @@
+package net.datenstrudel.fixtmagic
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.io.Serializable
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+
+
+class FixtureFactoryTest {
+
+    private val fixtureFactory: FixtureFactory = FixtureFactory.build{}
+
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
+
+    @Test
+    fun `it should create instance of simple class`() {
+        val res: SimpleClass = fixtureFactory.createInstance()
+        assertThat(res).isInstanceOf(SimpleClass::class.java)
+    }
+
+    @Test
+    fun `it should create instance of class with complex param`() {
+        val res: SimpleClassWithComplexParam = fixtureFactory.createInstance()
+        assertThat(res).isInstanceOf(SimpleClassWithComplexParam::class.java)
+        assertThat(res.paramA).isInstanceOf(SimpleClass::class.java)
+    }
+
+    @Test
+    fun `it should create instance of simple class with primitive params`() {
+        data class WithPrimitives(
+            val bo: Boolean,
+            val b: Byte,
+            val s: Short,
+            val i: Int,
+            val l: Long,
+            val d: Double,
+            val f: Float,
+            val c: Char,
+            val str: String
+        )
+
+        val res: WithPrimitives = fixtureFactory.createInstance()
+        assertThat(res).isInstanceOf(WithPrimitives::class.java)
+
+        log.info("$res")
+        assertThat(res.i).isBetween(fixtureFactory.randomIntRange.first, fixtureFactory.randomIntRange.last)
+        assertThat(res.str).hasSize(fixtureFactory.randomStringLength)
+    }
+
+    @Test
+    fun `it should be creatable by Builder with default params`(){
+        val res = FixtureFactory.build{}
+
+        assertThat(res.randomArrayLength).isEqualTo(10)
+        assertThat(res.randomCollectionSize).isEqualTo(10)
+        assertThat(res.randomStringLength).isEqualTo(10)
+        assertThat(res.randomIntRange).isEqualTo(IntRange(0, Int.MAX_VALUE))
+    }
+
+    @Test
+    fun `it should be creatable by Builder with user defined params`(){
+        val res = FixtureFactory.build{
+            randomArrayLength = 11
+            randomCollectionSize = 11
+            randomStringLength = 11
+            randomIntRange = IntRange(-10, 10)
+        }
+
+        assertThat(res.randomArrayLength).isEqualTo(11)
+        assertThat(res.randomCollectionSize).isEqualTo(11)
+        assertThat(res.randomStringLength).isEqualTo(11)
+        assertThat(res.randomIntRange).isEqualTo(IntRange(-10, 10))
+
+    }
+
+    data class GenericType<T>(val b: T)
+
+    data class ParentGenericType<T>(val a: ChildGenericType<T>)
+    data class ChildGenericType<T>(val a: T)
+
+    data class ParentMixedGenericType<A : Serializable, B>(val a: A, val b: ChildGenericType<B>)
+
+    @Nested
+    inner class Generics {
+
+        @Test
+        fun `it should create instance of generic class`() {
+            val res: GenericType<Short> = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(GenericType::class.java)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of generic class with many type params`() {
+            data class ManyGenericParamType<T, O, M>(val a: T, val b: O, val c: M)
+
+            val res: ManyGenericParamType<Long, String, Short> = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(ManyGenericParamType::class.java)
+            assertThat(res.b).isNotBlank()
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with generic param`() {
+            data class WithGenericParam(val a: GenericType<String>)
+
+            val res: WithGenericParam = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithGenericParam::class.java)
+            assertThat(res.a.b).isNotBlank()
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with generic complex param`() {
+            data class WithGenericParam(val a: GenericType<SimpleClass>)
+
+            val res: WithGenericParam = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithGenericParam::class.java)
+            assertThat(res.a.b).isInstanceOf(SimpleClass::class.java)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of generic class with nested type param`() {
+            val res: ParentGenericType<String> = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(ParentGenericType::class.java)
+            assertThat(res.a.a).isNotBlank()
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of generic class with nested and mixed type params`() {
+            val res: ParentMixedGenericType<String, String> = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(ParentMixedGenericType::class.java)
+            assertThat(res.a).isNotBlank()
+            assertThat(res.b.a).isNotBlank()
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of generic class with triple nested and mixed type params`() {
+            val res: ParentMixedGenericType<String, ChildGenericType<ChildGenericType<String>>> = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(ParentMixedGenericType::class.java)
+            assertThat(res.a).isNotBlank()
+            assertThat(res.b.a.a.a).isNotBlank()
+            log.info("$res")
+        }
+
+    }
+
+    @Nested
+    inner class ArrayTests {
+
+        @Test
+        fun `it should create instance of simple class with generic Array param`() {
+            class WithGenericArray(val a: Array<SimpleClass>)
+
+            val res: WithGenericArray = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithGenericArray::class.java)
+            assertThat(res.a).isInstanceOf(Array<SimpleClass>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with BooleanArray param`() {
+            class WithBooleanArray(val a: BooleanArray)
+
+            val res: WithBooleanArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithBooleanArray::class.java)
+            assertThat(res.a).isInstanceOf(BooleanArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Boolean param`() {
+            class WithArrayOfBoolean(val a: Array<Boolean>)
+
+            val res: WithArrayOfBoolean = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfBoolean::class.java)
+            assertThat(res.a).isInstanceOf(Array<Boolean>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with ByteArray param`() {
+            class WithByteArray(val a: ByteArray)
+
+            val res: WithByteArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithByteArray::class.java)
+            assertThat(res.a).isInstanceOf(ByteArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Byte param`() {
+            class WithArrayOfByte(val a: Array<Byte>)
+
+            val res: WithArrayOfByte = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfByte::class.java)
+            assertThat(res.a).isInstanceOf(Array<Byte>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with CharArray param`() {
+            class WithCharArray(val a: CharArray)
+
+            val res: WithCharArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithCharArray::class.java)
+            assertThat(res.a).isInstanceOf(CharArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Char param`() {
+            class WithArrayOfChar(val a: Array<Char>)
+
+            val res: WithArrayOfChar = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfChar::class.java)
+            assertThat(res.a).isInstanceOf(Array<Char>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with ShortArray param`() {
+            class WithShortArray(val a: ShortArray)
+
+            val res: WithShortArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithShortArray::class.java)
+            assertThat(res.a).isInstanceOf(ShortArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Short param`() {
+            class WithArrayOfShort(val a: Array<Short>)
+
+            val res: WithArrayOfShort = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfShort::class.java)
+            assertThat(res.a).isInstanceOf(Array<Short>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with Array of Int param`() {
+            class WithArrayOfInt(val a: Array<Int>)
+
+            val res: WithArrayOfInt = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithArrayOfInt::class.java)
+            assertThat(res.a).isInstanceOf(Array<Int>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with IntArray param`() {
+            class WithIntArray(val a: IntArray)
+
+            val res: WithIntArray = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithIntArray::class.java)
+            assertThat(res.a).isInstanceOf(IntArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with LongArray param`() {
+            class WithLongArray(val a: LongArray)
+
+            val res: WithLongArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithLongArray::class.java)
+            assertThat(res.a).isInstanceOf(LongArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Long param`() {
+            class WithArrayOfLong(val a: Array<Long>)
+
+            val res: WithArrayOfLong = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfLong::class.java)
+            assertThat(res.a).isInstanceOf(Array<Long>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with FloatArray param`() {
+            class WithFloatArray(val a: FloatArray)
+
+            val res: WithFloatArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithFloatArray::class.java)
+            assertThat(res.a).isInstanceOf(FloatArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Float param`() {
+            class WithArrayOfFloat(val a: Array<Float>)
+
+            val res: WithArrayOfFloat = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfFloat::class.java)
+            assertThat(res.a).isInstanceOf(Array<Float>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with DoubleArray param`() {
+            class WithDoubleArray(val a: DoubleArray)
+
+            val res: WithDoubleArray = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithDoubleArray::class.java)
+            assertThat(res.a).isInstanceOf(DoubleArray::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+
+        @Test
+        fun `it should create instance of simple class with Array of Double param`() {
+            class WithArrayOfDouble(val a: Array<Double>)
+
+            val res: WithArrayOfDouble = fixtureFactory.createInstance()
+            assertThat(res).isInstanceOf(WithArrayOfDouble::class.java)
+            assertThat(res.a).isInstanceOf(Array<Double>::class.java)
+                .hasSize(fixtureFactory.randomArrayLength)
+
+            log.info("$res")
+        }
+
+    }
+
+    @Nested
+    inner class CollectionTests {
+        @Test
+        fun `it should create instance of simple class with List param`() {
+            data class WithList(val l: List<String>)
+
+            val res: WithList = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithList::class.java)
+            assertThat(res.l).isInstanceOf(List::class.java)
+                .hasSize(fixtureFactory.randomCollectionSize)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with Set param`() {
+            data class WithSet(val l: Set<String>)
+
+            val res: WithSet = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithSet::class.java)
+            assertThat(res.l).isInstanceOf(Set::class.java)
+                .hasSize(fixtureFactory.randomCollectionSize)
+
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of simple class with Map param`() {
+            data class WithMap(val l: Map<String, Double>)
+
+            val res: WithMap = fixtureFactory.createInstance()
+
+            assertThat(res).isInstanceOf(WithMap::class.java)
+            assertThat(res.l).isInstanceOf(Map::class.java)
+                .hasSize(fixtureFactory.randomCollectionSize)
+
+            log.info("$res")
+        }
+
+    }
+
+    @Nested
+    inner class JavaTimeTests {
+
+        @Test
+        fun `it should create instance of class with Instant param`() {
+            data class WithInstant(val a: Instant)
+
+            val res: WithInstant = fixtureFactory.createInstance()
+            assertThat(res.a).isInstanceOf(Instant::class.java)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of class with LocalDate param`() {
+            data class WithLocalDate(val a: LocalDate)
+
+            val res: WithLocalDate = fixtureFactory.createInstance()
+            assertThat(res.a).isInstanceOf(LocalDate::class.java)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of class with LocalDateTime param`() {
+            data class WithLocalDateTime(val a: LocalDateTime)
+
+            val res: WithLocalDateTime = fixtureFactory.createInstance()
+            assertThat(res.a).isInstanceOf(LocalDateTime::class.java)
+            log.info("$res")
+        }
+
+        @Test
+        fun `it should create instance of class with ZonedDateTime param`() {
+            data class WithZonedDateTime(val a: ZonedDateTime)
+
+            val res: WithZonedDateTime = fixtureFactory.createInstance()
+            assertThat(res.a).isInstanceOf(ZonedDateTime::class.java)
+            log.info("$res")
+        }
+
+    }
+
+}
